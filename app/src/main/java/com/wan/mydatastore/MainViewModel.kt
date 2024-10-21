0f664a52707755c768250865066f6a2bf5c923ee
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2024-10-21 10:59:05 +0700

    Codelab Preferences DataStore

---
+++ app/src/main/java/com/wan/mydatastore/MainViewModel.kt
@@ -0,0 +1,31 @@
+package com.wan.mydatastore
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val pref: SettingPreferences) : ViewModel() {
+
+    fun getThemeSettings(): LiveData<Boolean> {
+        return pref.getThemeSetting().asLiveData()
+    }
+
+    fun saveThemeSetting(isDarkModeActive: Boolean) {
+        viewModelScope.launch { pref.saveThemeSetting(isDarkModeActive) }
+    }
+}
+
+class ViewModelFactory(private val pref: SettingPreferences) :
+    ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(pref) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
+}
